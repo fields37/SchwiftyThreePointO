0a216e85e1db2e21d3215af8e5fe95f0e2ade3d6
Gabriel Fields <gabef@mit.edu>
2016-10-10 07:42:28 -0400

    the last commit.... hopefully

---
+++ Assets/rendererthing.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class rendererthing : MonoBehaviour {
+
+    public RenderTexture targetTex;
+
+	// Use this for initialization
+	void Start () {
+        GetComponent<Camera>().targetTexture = targetTex;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
